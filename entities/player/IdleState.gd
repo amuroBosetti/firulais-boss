35adea48a5bcf7da6f7fed87a4300762507880bb
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-05-14 17:05:22 -0300

    corrige bug en movimiento

--- entities/player/IdleState.gd
+++ entities/player/IdleState.gd
@@ -1,5 +1,7 @@
 extends "res://entities/AbstractState.gd"
 
+class_name IdleState
+
 func handle_input(event:InputEvent):
 	if event.is_action_pressed("move_left") || event.is_action_pressed("move_right"):
 		emit_signal("finished", "walk")
@@ -9,6 +11,8 @@ func handle_input(event:InputEvent):
 var count = 0
 
 func update(delta:float):
+	if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+		emit_signal("finished", "walk")
 	parent._handle_deacceleration()
 	parent._apply_movement()
 	parent._handle_camera_movement_input()
