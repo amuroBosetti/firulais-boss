8f4ad0c7b885a06300346b298362fcd02a4c6916
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-05-17 21:58:23 -0300

    Remueve cosas que estaban en WIP

--- entities/world/Shadow.gd
+++ entities/world/Shadow.gd
@@ -1,12 +1,10 @@
 extends Sprite
 
 func _on_Area2D_body_entered(body):
-	print("entered")
 	if body is Player:
 		body.make_hidden()
 
 func _on_Area2D_body_exited(body):
-	print("exited")
 	if body is Player:
 		body.make_visible()
 
