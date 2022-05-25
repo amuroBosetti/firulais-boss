b3a64757970e961b7eddbbc071be05d0cd451061
Daniel <daniel.barberena10@gmail.com>
2022-05-25 17:05:23 -0300

    Mejoro lasers, agrego tool

--- Laser.gd
+++ Laser.gd
@@ -1,9 +1,24 @@
 extends Area2D
-
+tool
 
 export (int) var TIME_START 
 export (int) var CYCLETIME 
-onready var count:int = TIME_START * -1
+export (float) var SCALE_LENGTH = 1
+
+var count:int
+
+func _ready():
+	count = TIME_START * -1
+	$Sprite.scale.x = SCALE_LENGTH
+	$CollisionShape2D.scale.x = SCALE_LENGTH
+	$LaserEndA.global_position = $Sprite/Position2DA.global_position
+	$LaserEndB.global_position = $Sprite/Position2DB.global_position
+
+func _process(delta):
+	if Engine.editor_hint:
+		$Sprite.scale.x = SCALE_LENGTH
+		$LaserEndA.global_position = $Sprite/Position2DA.global_position
+		$LaserEndB.global_position = $Sprite/Position2DB.global_position
 
 func timeout():
 	count += 1 
@@ -12,11 +27,11 @@ func timeout():
 	elif count == CYCLETIME * 2:
 		enable_laser(false)
 		count = 0
-		
+
 func enable_laser(boolean:bool):
 		$CollisionShape2D.set_deferred("disabled", !boolean)
 		$Sprite.set_deferred("visible", boolean)
-	
+
 func _on_Laser_body_entered(body):
 	if body is Player:
 		body.respawn_player()
