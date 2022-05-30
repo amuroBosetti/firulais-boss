d3b1d9cbf0c41a127367ea02e0e626874351a0fc
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-05-30 08:53:36 -0300

    Agrega variable de zoom de salida a camaras

--- SnapCamera.gd
+++ SnapCamera.gd
@@ -3,16 +3,17 @@ tool
 
 signal snapCamera
 
-export (Vector2) var ZOOM:Vector2 = Vector2(1,1)
+export (Vector2) var ENTRANCE_ZOOM:Vector2 = Vector2(1,1)
+export (Vector2) var EXIT_ZOOM:Vector2 = Vector2(1,1)
 export (Vector2) var SCALE:Vector2 = Vector2(1,1) setget _set_scale
 
 func _on_SnapCamera_body_entered(body):
 	if body is Player:
-		emit_signal("snapCamera", $CameraPosition.global_position, ZOOM, false)
+		emit_signal("snapCamera", $CameraPosition.global_position, ENTRANCE_ZOOM, false)
 
 func _on_SnapCamera_body_exited(body):
 	if body is Player:
-		emit_signal("snapCamera", body.position, Vector2(1,1), true)
+		emit_signal("snapCamera", body.position, EXIT_ZOOM, true)
 
 func _set_scale(scale_vector):
 	SCALE = scale_vector
