b7478a3148e719db622fda3e0412be656bb1723f
Daniel <daniel.barberena10@gmail.com>
2022-06-05 14:07:05 -0300

    agrego fade del logo

--- FirulaisLogo.gd
+++ FirulaisLogo.gd
@@ -1,5 +1,17 @@
 extends Node
 
+onready var fade = $Fade/ScreenCover
+onready var fade_tween = $Fade/Tween
+
 func _ready():
-	yield(get_tree().create_timer(3), "timeout")
+	fade.color.a = 0
+	fade.hide()
+	yield(get_tree().create_timer(4), "timeout")
+	fade_to_black()
+	yield(get_tree().create_timer(2), "timeout")
 	get_tree().change_scene("res://InGame.tscn")
+
+func fade_to_black():
+	fade.show()
+	fade_tween.interpolate_property(fade, "color", fade.color, Color.black, 1)
+	fade_tween.start()
