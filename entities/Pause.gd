d4ff000e6b5af31255b085fd297c52c85156f791
Daniel <daniel.barberena10@gmail.com>
2022-06-11 17:04:39 -0300

    pausa

---
+++ entities/Pause.gd
@@ -0,0 +1,17 @@
+extends Node
+
+onready var tint = $CanvasLayer/ColorRect
+onready var label = $CanvasLayer/Label
+
+var pause = true
+
+func _ready():
+	tint.hide()
+	label.hide()
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = pause
+		tint.visible = pause
+		label.visible = pause
+		pause = !pause
