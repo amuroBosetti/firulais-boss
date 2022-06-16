1a0b839784f732f4e8f5c1fd53b3837e6d3d6fa3
Daniel <daniel.barberena10@gmail.com>
2022-06-15 22:30:01 -0300

    fix velocidad caida hang

--- entities/player/HangState.gd
+++ entities/player/HangState.gd
@@ -2,6 +2,7 @@ extends "res://entities/AbstractState.gd"
 
 func enter():
 	parent.play_animation("hang")
+	parent.velocity.y = 0
 
 func handle_input(event:InputEvent):
 	if event.is_action_pressed("jump"):
