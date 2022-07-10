6ae2f5b056ad59ad4c1d1269640c21b0261387bc
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-07-10 16:36:43 -0300

    Agrega animacion colgado y mirando para el otro lado

--- entities/player/HangState.gd
+++ entities/player/HangState.gd
@@ -4,13 +4,19 @@ func enter():
 	parent.play_animation("hang")
 	parent.velocity.y = 0
 
+func update(delta):
+	if !_should_look_to_other_side(Input.is_action_pressed("move_left"), Input.is_action_pressed("move_right")):
+		parent.play_animation("hang")
+
 func handle_input(event:InputEvent):
 	if event.is_action_pressed("jump"):
 		emit_signal("finished", "jump_up")
 	if event.is_action_pressed("crouch"):
 		emit_signal("finished", "jump_down")
 #	COntroles para bajar si apretas para el lado contrario
-#	if parent.hanging_direction == 1 and event.is_action_pressed("move_left"):
-#		emit_signal("finished", "jump_down")
-#	if parent.hanging_direction == -1 and event.is_action_pressed("move_right"):
-#		emit_signal("finished", "jump_down")
+	if _should_look_to_other_side(event.is_action_pressed("move_left"), event.is_action_pressed("move_right")):
+		parent.play_animation("hang_looking_to_other_side")
+
+func _should_look_to_other_side(is_pressing_left:bool, is_pressing_right:bool) -> bool:
+	return (parent.hanging_direction == 1 and is_pressing_left) or \
+		(parent.hanging_direction == -1 and is_pressing_right)
