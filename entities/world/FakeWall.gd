d36f38ec514347bb8d89cc315c330091a1bb805b
Mauro Bosetti <mauro.bosetti@10pines.com>
2022-07-11 08:49:59 -0300

    Agrega indicador de robo de cuadro + pared falsa

--- entities/world/FakeWall.gd
+++ entities/world/FakeWall.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 onready var tween:Tween = $Tween
 onready var wall:ColorRect = $Area2D/CollisionShape2D/ColorRect
+onready var particles:Particles2D = $Particles2D
 
 func _ready():
 	wall.color.a = 1
 
 func _on_Area2D_body_entered(body):
 	if body is Player:
+		particles.emitting = false
 		tween.interpolate_property(wall, "color:a", wall.color.a, 0.2, 1)
 		tween.start()
 
 func _on_Area2D_body_exited(body):
 	if body is Player:
+		particles.emitting = true
 		tween.interpolate_property(wall, "color:a", wall.color.a, 1, 1)
 		tween.start()
